ad829bc5f54b2cd5148697aec9882accacc8d09c
Rubens Leme <rubenss.leme@gmail.com>
2019-09-10 21:50:35 -0300

    inicio do projeto

---
+++ Wanvan/app/src/main/java/com/example/wanvan/ControladorTelaSobre.java
@@ -0,0 +1,14 @@
+package com.example.wanvan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ControladorTelaSobre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_sobre);
+    }
+}
